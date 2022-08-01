8e2b5553933d2031acbf5468e7a150957259e85c
Carlos Simon <git@csimon.dev>
2022-08-01 10:43:20 +0200

    add tests and add item types

--- swift/Sources/GildedRose/Item.swift
+++ swift/Sources/GildedRose/Item.swift
@@ -1,12 +1,34 @@
+private enum ItemType: String, RawRepresentable {
+    case regular
+    case agedBrie
+    case sulfuras
+    case backstagePasses
+    
+    init?(rawValue: String) {
+        switch rawValue{
+        case "Aged Brie":
+            self = .agedBrie
+        case "Backstage passes to a TAFKAL80ETC concert":
+            self = .backstagePasses
+        case "Sulfuras, Hand of Ragnaros":
+            self = .sulfuras
+        default:
+            self = .regular
+        }
+    }
+}
+
 public class Item {
     public var name: String
     public var sellIn: Int
     public var quality: Int
-
+    private var itemType: ItemType
+    
     public init(name: String, sellIn: Int, quality: Int) {
         self.name = name
         self.sellIn = sellIn
         self.quality = quality
+        self.itemType = ItemType(rawValue: name) ?? .regular
     }
 }
 
--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -2,10 +2,119 @@
 import XCTest
 
 class GildedRoseTests: XCTestCase {
-    func testFoo() throws {
-        let items = [Item(name: "foo", sellIn: 0, quality: 0)]
+    func testQualityDoesntIsNegative() throws {
+        let items = [Item(name: "foo", sellIn: 1, quality: 0)]
         let app = GildedRose(items: items)
         app.updateQuality()
-        XCTAssertEqual(app.items[0].name, "fixme")
+        XCTAssertEqual(app.items[0].quality, 0)
     }
+    
+    func testUpdateQualityRegualarItem() throws {
+        let items = [Item(name: "foo", sellIn: 1, quality: 1)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].quality, 0)
+    }
+    
+    func testUpdateDegradationAfterTwoDayPassed() throws {
+        let items = [Item(name: "foo", sellIn: 1, quality: 5)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].quality, 2)
+    }
+    func testUpdateDegradationAfterDayPassed() throws {
+        let items = [Item(name: "foo", sellIn:-1, quality: 5)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].quality, 3)
+    }
+    
+    func testAgeBrieIncreaseQualityTime() throws {
+        let items = [Item(name: "Aged Brie", sellIn: 5, quality: 5)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].quality, 6)
+    }
+    
+    func testAgeBrieIncreaseQualityTimeLimitDay() throws {
+        let items = [Item(name: "Aged Brie", sellIn: 1, quality: 5)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].quality, 6)
+    }
+    func testAgeBrieIncreaseQualityTimeStopsAt50() throws {
+        let items = [Item(name: "Aged Brie", sellIn: 5, quality: 50)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].quality, 50)
+    }
+    func testDayPassedIncreaseBrieQualityUntil50OnLimit() throws {
+        let items = [Item(name: "Aged Brie", sellIn: -1, quality: 50)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].quality, 50)
+    }
+    func testDayPassedIncreaseBrieQualityUntil50() throws {
+        let items = [Item(name: "Aged Brie", sellIn: -1, quality: 49)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].quality, 50)
+    }
+    
+    func testDayPassedIncreaseDoubleSellInPassed() throws {
+        let items = [Item(name: "Aged Brie", sellIn: -1, quality: 1)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].quality, 3)
+    }
+    func testSulfurasAlwaysKeepTheSameQuality() throws {
+        let items = [Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 5, quality: 20)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].quality, 20)
+    }
+    func testSulfurasAlwaysKeepTheSameQualityDayPassed() throws {
+        let items = [Item(name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 20)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].quality, 20)
+    }
+    
+    func testBackstageQualityIncreaseBy2_10Days() throws {
+        let items = [Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 5)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].quality, 7)
+    }
+    func testBackstageQualityIncreaseBy2_6Days() throws {
+        let items = [Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 6, quality: 5)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].quality, 7)
+    }
+    
+    func testBackstageQualityIncreaseBy2_5Days() throws {
+        let items = [Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 5)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].quality, 8)
+    }
+    
+    func testBackstageQualityIncreaseBy3_1Days() throws {
+        let items = [Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 1, quality: 5)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].quality, 8)
+    }
+    
+    func testBackstageQualityDrops0_0Days() throws {
+        let items = [Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 0, quality: 5)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].quality, 0)
+    }
+    
+    
+    
 }
